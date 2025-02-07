fed3ff6a2855fbc818d280240a4f54937ec490aa
yellow51 <57696915+yellow5111@users.noreply.github.com>
2025-02-06 19:38:26 -0700

    Init Upl
    This requires 0Harmony.dll.
    
    Visit https://se2.yellow51.dev for more info.

---
+++ SpaceEngineers2NoPCU/Patches/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using Keen.Game2.Simulation.GameSystems.PCU;
+using Keen.Game2.Simulation.GameSystems.Ownership;
+using System.Reflection;
+
+namespace SpaceEngineers2NoPCU
+{
+    [HarmonyPatch(typeof(PCUSessionComponent), "HasPCU")]
+    public class PCUSessionComponent_hasPCU_patch
+    {
+        public static bool Prefix(ref bool __result, PCUSessionComponent __instance, IdentityId owner, int requiredPCU)
+        {
+            
+            __result = true;
+
+            
+            var maxGlobalPCUField = typeof(PCUSessionComponent).GetField("_maxGlobalPCU", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (maxGlobalPCUField != null)
+            {
+                maxGlobalPCUField.SetValue(__instance, 999999999);
+            }
+
+            
+            var globalPCUField = typeof(PCUSessionComponent).GetField("_globalPCU", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (globalPCUField != null)
+            {
+                globalPCUField.SetValue(__instance, 0);
+            }
+
+            
+            var playerPCUField = typeof(PCUSessionComponent).GetField("_playerPCU", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (playerPCUField != null)
+            {
+                var playerPCU = playerPCUField.GetValue(__instance) as PCUPerPlayerData;
+                if (playerPCU != null)
+                {
+                    
+                    var maxPlayerPCUField = typeof(PCUPerPlayerData).GetField("_maxPCU", BindingFlags.NonPublic | BindingFlags.Instance);
+                    if (maxPlayerPCUField != null)
+                    {
+                        maxPlayerPCUField.SetValue(playerPCU, 999999999);
+                    }
+
+                    
+                    var playerPCUUsageField = typeof(PCUPerPlayerData).GetField("_pCU", BindingFlags.NonPublic | BindingFlags.Instance);
+                    if (playerPCUUsageField != null)
+                    {
+                        playerPCUUsageField.SetValue(playerPCU, 0);
+                    }
+                }
+            }
+
+            return false; 
+        }
+    }
+}
---
+++ SpaceEngineers2NoPCU/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Keen.Game2.Game.Plugins;
+using Keen.Game2.Simulation.GameSystems.PCU;
+using Keen.VRage.Library.Diagnostics;
+using System.Reflection;
+
+namespace SpaceEngineers2NoPCU
+{
+    public class Plugin : IPlugin
+    {
+        public const string Name = "Space Engineers 2 - NO PCU";
+
+        public Plugin()
+        {
+            Log.Default.WriteLine($"[{Name}] loaded...");
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+            Harmony harmony = new Harmony(Name);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Log.Default.WriteLine($"[{Name}] applied...");
+        }
+    }
+}
